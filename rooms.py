d100123845e58b575474cee55099cb9589ea11e0
Kevin Lueke <kevinlueke66@gmail.com>
2020-02-05 21:15:44 -0500

    New dictionary for tasks, Develop cabin_bathroom
    cabin_bathroom now has option to take a drug

--- rooms.py
+++ rooms.py
@@ -2,9 +2,16 @@
 from sys import exit
 from random import randint
 from textwrap import dedent
+from player_stats import *
 prompt = "~> "
 valid_move = "Please choose a valid option -> (h, j, k, l)\n~>"
 # have classes pass through a parent to check if they can be entered
+
+# Tasks to be completed
+
+completed = {
+        'drugs': False
+        }
 
 class Introduction(object):
     def enter(self):
@@ -18,6 +25,10 @@ class Introduction(object):
             k will move forward
             l will move right
             ---------------------------------------
+            When presented with a choice to do something:
+            yes will agree to something
+            no will disagree to something
+            ---------------------------------------
             There's a magical door right in front of you
             """))
 
@@ -25,10 +36,9 @@ class Introduction(object):
 
         while True:
 
-            if action == 'forward':
+            if action == 'k':
                 print("The door you came out of slammed shut behind you and disappeared")
                 return 'cabin_main_room'
-        
             else:
                 action = input(valid_move)
 
@@ -38,13 +48,35 @@ class CabinMainRoom(object):
         print(dedent("""
             You are standing in the entrance of a cabin
             To your right there is a bathroom
+            Behind you is a bedroom
+            In front of you is a door to leave the cabin
             """))
-    
+
+        action = input(prompt)
+
+        while True:
+            if action == 'k':
+                print("develop")
+            elif action == 'l':
+                return 'cabin_bathroom'
+            elif action == 'j':
+                return 'cabin_bedroom'
+            else:
+                action = input(valid_move)
 class Bathroom(object):
     
     def enter(self):
-        print("working 2")
-        return 'cabin_bedroom'
+        if completed['drugs'] == False:
+            print(dedent("""
+                There is a drug in the bathroom that is only precribed to people with cancer
+                Do you take it?
+                Taking it will decrease your morality by 5
+                """))
+            action = input(prompt)
+            if action == 'yes':
+                completed['drugs'] = True
+                player.morality -= 5
+                print(player.morality)
 
 class CabinBedroom(object):
 
